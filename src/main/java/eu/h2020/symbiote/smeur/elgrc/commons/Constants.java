711e47ed4c67eae3b626484000126da0ad20c8d5
Joao Amsellem <jamsellem@ubiwhere.com>
2018-06-20 12:20:23 +0100

    constants

---
+++ src/main/java/eu/h2020/symbiote/smeur/elgrc/commons/Constants.java
@@ -0,0 +1,14 @@
+package eu.h2020.symbiote.smeur.elgrc.commons;
+
+import scala.collection.immutable.Stream;
+
+public class Constants {
+
+
+	public static final String GET_ALL_ROUTES = "/routes";
+	public static final String GET_ROUTES_FILTERED = "/routes/filter";
+
+
+	private Constants(){
+	}
+}
